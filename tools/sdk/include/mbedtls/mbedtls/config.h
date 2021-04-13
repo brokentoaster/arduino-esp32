0e91bf84bb336bd1775ba733444ece1d592cb4ec
Nick Lott <nick.lott@gmail.com>
2021-04-13 21:06:51 +1200

    Support  TLS 1.2 ONLY.

--- tools/sdk/include/mbedtls/mbedtls/config.h
+++ tools/sdk/include/mbedtls/mbedtls/config.h
@@ -1534,7 +1534,7 @@
  *
  * Comment this macro to disable support for TLS 1.0
  */
-#define MBEDTLS_SSL_PROTO_TLS1
+//#define MBEDTLS_SSL_PROTO_TLS1
 
 /**
  * \def MBEDTLS_SSL_PROTO_TLS1_1
@@ -1546,7 +1546,7 @@
  *
  * Comment this macro to disable support for TLS 1.1 / DTLS 1.0
  */
-#define MBEDTLS_SSL_PROTO_TLS1_1
+//#define MBEDTLS_SSL_PROTO_TLS1_1
 
 /**
  * \def MBEDTLS_SSL_PROTO_TLS1_2
